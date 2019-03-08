d20ff47dfd9182fc8c0f711f0888c97689b9f819
Peter Tran <peter.tran@109MAC009.local>
2019-03-08 12:08:56 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-fluid">
+        <app-header></app-header>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+
+    export default {
+        components: {
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+    .container-fluid {
+        background-color: #f7f7f7;
+        margin-top: 70px;
+       }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 class="text-center">Stock Trader</h1>
+        <nav class="navbar">
+            <a class="nav-stock-link">Portfolio</a>
+            <a class="nav-stock-link">Stocks</a>
+            <a class="nav-stock-link">End Day</a>
+            <a class="nav-stock-link">Save & Load</a>
+            <p>Funds</p>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    h1 {
+        font-size: 50px;
+    }
+
+    .nav-stock-link {
+        color:darkgray;
+        font-size: 20px;
+    }
+</style>
